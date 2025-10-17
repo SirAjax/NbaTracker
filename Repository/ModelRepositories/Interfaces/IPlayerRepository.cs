4e8a2206bc56786b5438e03d8d26e653710e8d5f
browski.rios <browski.rios@gmail.com>
2025-10-16 21:55:06 -0400

    PlayerRepository setup along with DbSet model

---
+++ Repository/ModelRepositories/Interfaces/IPlayerRepository.cs
@@ -0,0 +1,9 @@
+﻿using NbaTracker.Repository.DbSets;
+using NbaTracker.Repository.Interfaces;
+
+namespace NbaTracker.Repository.ModelRepositories.Interfaces
+{
+    public interface IPlayerRepository : IGenericRepository<Player>
+    {
+    }
+}
---
+++ Repository/ModelRepositories/PlayerRepository.cs
@@ -0,0 +1,12 @@
+﻿using NbaTracker.Repository.DbSets;
+using NbaTracker.Repository.ModelRepositories.Interfaces;
+
+namespace NbaTracker.Repository.ModelRepositories
+{
+    public class PlayerRepository : GenericRepository<Player>, IPlayerRepository
+    {
+        public PlayerRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+        {
+        }
+    }
+}
