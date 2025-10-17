94a4b963ac9abbc553e2799794bb1ddb40b6a92e
browski.rios <browski.rios@gmail.com>
2025-10-16 21:54:35 -0400

    RepositoryContext setup

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NbaTracker.Repository.DbSets;
+
+namespace NbaTracker.Repository
+{
+    public class RepositoryContext(DbContextOptions<RepositoryContext> options) : DbContext(options)
+    {
+        public DbSet<Player> Players { get; set; }
+    }
+}
